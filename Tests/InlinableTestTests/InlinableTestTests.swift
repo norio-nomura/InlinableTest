8c4dd3f1e1b2f441e9adca78eea8ca77cb45a778
Norio Nomura <norio.nomura@gmail.com>
2019-03-05 12:27:28 +0900

    Add checking results in tests

--- Tests/InlinableTestTests/InlinableTestTests.swift
+++ Tests/InlinableTestTests/InlinableTestTests.swift
@@ -2,19 +2,22 @@ import XCTest
 import InlinableTest
 
 final class InlinableTestTests: XCTestCase {
-    let ranges = (0..<100000).map { NSRange(location: $0, length: $0) }
+    let counts = 100000
+    lazy var ranges = (0..<counts).map { NSRange(location: $0, length: $0) }
     
     func testInlined() {
         let myStruct = MyStruct.init(ranges: ranges)
         measure {
-            _ = myStruct.inlinedRanges("")
+            let myRanges = myStruct.inlinedRanges("")
+            XCTAssertEqual(myRanges.count, counts)
         }
     }
 
     func testNotInlined() {
         let myStruct = MyStruct.init(ranges: ranges)
         measure {
-            _ = myStruct.ranges("")
+            let myRanges = myStruct.ranges("")
+            XCTAssertEqual(myRanges.count, counts)
         }
     }
 }
